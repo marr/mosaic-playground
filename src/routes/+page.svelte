7601bc0eb8a4cfc32a0079a0dc80fb7b5a23bacd
David Marr <david.marr@contextlabs.com>
2023-11-01 11:22:38 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import { onMount } from 'svelte';
+	import vg from '$lib/vg';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+
+	onMount(() => {
+		document.querySelector('#output')?.appendChild(vg);
+	});
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Stock Closing Prices</h1>
+		<div id="output" class="text-primary-500" />
+		<p>Start by exploring:</p>
+		<ul>
+			<li>
+				<code class="code">/src/routes/+layout.svelte</code> - barebones layout, the CSS import order
+				is critical!
+			</li>
+			<li>
+				<code class="code">/src/app.postcss</code> - minimal css to make the page full screen, may not
+				be relevant for your project
+			</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
+		</ul>
+	</div>
+</div>
